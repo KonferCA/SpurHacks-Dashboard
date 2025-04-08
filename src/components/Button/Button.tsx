5ded6e660f000aa186ae5376664563344018a36c
Benjamin Roman Perez <59741853+roman20203@users.noreply.github.com>
2025-04-08 12:31:15 -0400

    Simplified the Button ChakraUI implementation

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,30 +2,18 @@ import { Button as ChakraButton } from "@chakra-ui/react";
 import { getButtonStyles } from "./Button.styles";
 import type { ButtonProps } from "./Button.type";
 
-interface ExtendedButtonProps extends ButtonProps {
-    //Adds Icon and iconPosition props to the Button component
-    icon?: React.ReactNode;
-    onlyIcon?: boolean;
-    iconPosition?: "left" | "right";
-}
 
-export const Button: React.FC<ExtendedButtonProps> = ({
+export const Button: React.FC<ButtonProps> = ({
     children, 
     intent, 
     className, 
-    icon, 
-    onlyIcon = false, 
-    iconPosition = "left",
     ...props
 }) => {
     const buttonClass = getButtonStyles({ intent, className });
 
     return (
         <ChakraButton className={buttonClass} {...props}>
-            {onlyIcon && icon}
-            {!onlyIcon && iconPosition === "left" && icon}
-            {!onlyIcon && children}
-            {!onlyIcon && iconPosition === "right" && icon}
+            {children}
         </ChakraButton>
     );
 };
