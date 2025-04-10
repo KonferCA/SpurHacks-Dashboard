05db96913d209b9a8dc7bf19bdb9a93f2277f454
jc <46619361+juancwu@users.noreply.github.com>
2025-04-10 00:54:59 -0400

    feat: update provider mocks

--- src/providers/__mocks__/index.tsx
+++ src/providers/__mocks__/index.tsx
@@ -1,2 +1,16 @@
+// AuthProvider mocks
 export const mockUseAuth = vi.fn();
 export const useAuth = mockUseAuth;
+export const mockUseUser = vi.fn();
+export const useUser = mockUseUser;
+
+// RoutesProvider mocks
+export const mockUseRouter = vi.fn();
+export const useRouter = mockUseRouter;
+export const mockUseHeaderInfo = vi.fn();
+export const useHeaderInfo = mockUseHeaderInfo;
+export const mockUseRouteDefinitions = vi.fn();
+export const useRouteDefinitions = mockUseRouteDefinitions;
+
+export const mockUseApplications = vi.fn();
+export const useApplications = mockUseApplications;
