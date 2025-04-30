35b5e494ab4807fd922feb9d2cbc069241d87cc1
Sharib Kabani <ksharib05@gmail.com>
2025-04-30 12:45:36 -0400

    updated spacing

--- src/components/TextInput/TextInput.styles.ts
+++ src/components/TextInput/TextInput.styles.ts
@@ -3,62 +3,62 @@ import type { ClassProp } from "class-variance-authority/types";
 import { twMerge } from "tailwind-merge";
 
 const textInputStyles = cva(
-	[
-		"block w-full py-2 px-3 text-gray-900 placeholder:text-sageGray", // base
-		"focus:ring-2 focus:ring-inset focus:ring-[#00848C]", // focus styles
-	],
-	{
-		variants: {
-			invalid: {
-				true: "text-red-500 ring-red-500 ring-2 placeholder:text-red-300 focus:ring-red-500",
-			},
-			background: {
-				default: "bg-gray-50",
-				transparent: "bg-white/20",
-				none: "",
-			}
-		},
-		defaultVariants: {
-			invalid: false,
-		},
-	},
+    [
+        "block w-full py-2 px-3 text-gray-900 placeholder:text-sageGray", // base
+        "focus:ring-2 focus:ring-inset focus:ring-[#00848C]", // focus styles
+    ],
+    {
+        variants: {
+            invalid: {
+                true: "text-red-500 ring-red-500 ring-2 placeholder:text-red-300 focus:ring-red-500",
+            },
+            background: {
+                default: "bg-gray-50",
+                transparent: "bg-white/20",
+                none: "",
+            },
+        },
+        defaultVariants: {
+            invalid: false,
+        },
+    }
 );
 
 export type TextInputStylesProps = VariantProps<typeof textInputStyles>;
 
 export function getTextInputStyles(
-	opts: TextInputStylesProps & ClassProp,
+    opts: TextInputStylesProps & ClassProp
 ): string {
-	return twMerge(textInputStyles(opts));
+    return twMerge(textInputStyles(opts));
 }
 
 export const getTextInputLabelStyles = cva(
-	"block font-medium leading-5 text-white text-md",
-	{
-		variants: {
-			srLabel: {
-				true: "sr-only",
-			},
-		},
-		defaultVariants: {
-			srLabel: false,
-		},
-	},
+    "block font-medium leading-5 text-white text-md",
+    {
+        variants: {
+            srLabel: {
+                true: "sr-only",
+            },
+        },
+        defaultVariants: {
+            srLabel: false,
+        },
+    }
 );
 
 const textInputDescriptionStyles = cva("mt-2 text-sm text-sageGray", {
-	variants: {
-		invalid: {
-			true: "text-red-500",
-		},
-	},
-	defaultVariants: {
-		invalid: false,
-	},
+    variants: {
+        invalid: {
+            true: "text-red-500",
+        },
+    },
+    defaultVariants: {
+        invalid: false,
+    },
 });
 
 export function getTextInputDescriptionStyles(
-	opts: VariantProps<typeof textInputStyles>,
+    opts: VariantProps<typeof textInputStyles>
 ) {
-	return twMerge(textInputDescriptionStyles(opts));
+    return twMerge(textInputDescriptionStyles(opts));
 }
