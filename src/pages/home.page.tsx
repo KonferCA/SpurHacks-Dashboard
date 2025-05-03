93cc80e8f3a0dd56c1d8ab01b6942e074d503946
jc <46619361+juancwu@users.noreply.github.com>
2025-05-03 17:29:52 -0400

    update home page content and styles

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,13 +1,7 @@
-import { GoldenHawk, IpadKidHawks } from "@/assets";
-import { toaster } from "@/components/ui/toaster";
 import { useApplications } from "@/hooks/use-applications";
-import { useAuth } from "@/providers";
 import { paths } from "@/providers/RoutesProvider/data";
-import { withdrawRSVP } from "@/services/firebase/rsvp";
 import { Button } from "@chakra-ui/react";
-import { Accordion, Card, Modal, PageWrapper, SocialIcons } from "@components";
-import { faqs, importantDateTimes, sponsors } from "@data";
-import { useState } from "react";
+import { PageWrapper } from "@components";
 import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
@@ -16,32 +10,32 @@ const HomePage = () => {
 
 	return (
 		<PageWrapper>
-			<section className="homepage grid gap-4">
-				<div className="grid xl:grid-cols-12 gap-4">
-					{deadlines.inRange && (
-						<div>
-							<p>Applications for SpurHacks 2025 are open now!</p>
-							<Button
-								colorPalette="brand"
-								onClick={() => navigate(paths.apply)}
-								size="lg"
-								className="mt-2"
-							>
-								Apply Now
-							</Button>
-						</div>
-					)}
-					{deadlines.beforeStart && (
-						<div>
-							<p>Applications for SpurHacks 2025 open on May 5, 2025.</p>
-						</div>
-					)}
-					{deadlines.afterClose && (
-						<div>
-							<p>Applications have now closed for SpurHacks 2025.</p>
-						</div>
-					)}
-				</div>
+			<section className="">
+				{deadlines.inRange && (
+					<div>
+						<p>Applications for SpurHacks 2025 are open now!</p>
+						<Button
+							color="black"
+							colorScheme="brand"
+							onClick={() => navigate(paths.apply)}
+							size="lg"
+							rounded="full"
+							className="mt-2"
+						>
+							Apply Now
+						</Button>
+					</div>
+				)}
+				{deadlines.beforeStart && (
+					<div>
+						<p>Applications for SpurHacks 2025 open on May 5, 2025.</p>
+					</div>
+				)}
+				{deadlines.afterClose && (
+					<div>
+						<p>Applications have now closed for SpurHacks 2025.</p>
+					</div>
+				)}
 			</section>
 		</PageWrapper>
 	);
