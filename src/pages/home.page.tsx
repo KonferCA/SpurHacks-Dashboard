ac1714268ebad00ca135a832cc6a31649962ecbf
jc <46619361+juancwu@users.noreply.github.com>
2025-05-14 17:33:42 -0400

    add extra statuses to hacker status card in home page

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -6,6 +6,7 @@ import { useApplications } from "@/hooks/use-applications";
 import { paths } from "@/providers/RoutesProvider/data";
 import { getTypeforms } from "@/services/firebase/misc";
 import {
+	Badge,
 	Box,
 	Button,
 	Card,
@@ -24,13 +25,8 @@ import { useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-	const { deadlines, applications } = useApplications();
+	const { deadlines, current: currentApplication } = useApplications();
 	const navigate = useNavigate();
-
-	const hasApplied = useMemo(() => {
-		const app = applications.find((app) => app.hackathonYear === "2025");
-		return app && app.applicationStatus === "pending";
-	}, [applications]);
 
 	const { data: typeforms, isLoading: isLoadingTypeforms } = useQuery({
 		queryKey: ["typeforms"],
@@ -57,65 +53,148 @@ const HomePage = () => {
 		<PageWrapper>
 			<Box as="section" spaceY="1.5rem">
 				<Flex gap="1.5rem" flexWrap={{ base: "wrap", xl: "nowrap" }}>
-					<Card.Root {...cardStyles}>
-						<Card.Header>
-							<Card.Title>Hacker Status</Card.Title>
-						</Card.Header>
-						<Card.Body>
-							{/* <Heading mb="1rem">Hacker Status</Heading> */}
-							{deadlines.inRange && (
-								<Box spaceY="1rem">
-									<Flex alignItems="center" gapX="1rem">
-										<Text color="fg.muted">
-											{hasApplied
-												? "Application submitted"
-												: "Application not submitted"}
-										</Text>
-										<Icon
+					{/* Only render if no submission or application status is not accepted and has not rsvp'd */}
+					{(!currentApplication ||
+						currentApplication.applicationStatus !== "accepted" ||
+						!currentApplication.rsvp) && (
+						<Card.Root {...cardStyles}>
+							<Card.Header flexDir="row" justifyContent="space-between">
+								<Card.Title>Hacker Status</Card.Title>
+								{!!currentApplication &&
+									currentApplication.applicationStatus !== "rejected" && (
+										<Badge
+											bg={
+												currentApplication.applicationStatus === "pending"
+													? "bg.hover"
+													: "transparent"
+											}
+											borderStyle={
+												currentApplication.applicationStatus === "pending"
+													? "none"
+													: "solid"
+											}
+											borderWidth="2px"
+											borderColor={
+												currentApplication.applicationStatus === "pending"
+													? "transparent"
+													: "green.400"
+											}
+											color={
+												currentApplication.applicationStatus === "pending"
+													? "fg.muted"
+													: "green.400"
+											}
 											size="lg"
-											color={hasApplied ? "green.400" : "yellow.400"}
+											rounded="full"
+											textTransform="uppercase"
 										>
-											{hasApplied ? <CheckCircle /> : <WarningCircle />}
-										</Icon>
-									</Flex>
-									<Text color="fg.muted">
-										{hasApplied
-											? "You’ll receive a confirmation on your application status in the email you provided."
-											: "Bring your ideas to life, build something bold, and collaborate with passionate peers! All skill levels are welcome."}
-									</Text>
-								</Box>
-							)}
-							{deadlines.beforeStart && (
-								<Box>
-									<Text>
-										Applications for SpurHacks 2025 open on{" "}
-										{deadlines.startDateStr}.
-									</Text>
-								</Box>
-							)}
-							{deadlines.afterClose && (
-								<Box>
-									<Text>Applications have now closed for SpurHacks 2025.</Text>
-								</Box>
-							)}
-						</Card.Body>
-						<Card.Footer>
-							{!hasApplied && (
-								<Button
-									color="black"
-									colorScheme="brand"
-									onClick={() => navigate(paths.apply)}
-									marginLeft="auto"
-									size="lg"
-									rounded="full"
-									marginTop="1rem"
-									textTransform="uppercase"
-								>
-									Apply Now
-								</Button>
-							)}
-						</Card.Footer>
-					</Card.Root>
+											{currentApplication.applicationStatus}
+										</Badge>
+									)}
+							</Card.Header>
+							<Card.Body>
+								{deadlines.inRange && (
+									<Box spaceY="1rem">
+										<Flex alignItems="center" gapX="1rem">
+											<Text color="fg.muted">
+												{!!currentApplication
+													? "Application submitted"
+													: "Application not submitted"}
+											</Text>
+											<Icon
+												size="lg"
+												color={
+													!!currentApplication ? "green.400" : "yellow.400"
+												}
+											>
+												{!!currentApplication ? (
+													<CheckCircle />
+												) : (
+													<WarningCircle />
+												)}
+											</Icon>
+										</Flex>
+										<Text color="fg.muted">
+											{!!currentApplication
+												? "You’ll receive a confirmation on your application status in the email you provided."
+												: "Bring your ideas to life, build something bold, and collaborate with passionate peers! All skill levels are welcome."}
+										</Text>
+									</Box>
+								)}
+								{deadlines.afterClose && (
+									<Box>
+										{!currentApplication && (
+											<Text color="fg.muted">
+												Applications have now closed for SpurHacks 2025.
+											</Text>
+										)}
+										{!!currentApplication &&
+											currentApplication.applicationStatus === "accepted" &&
+											!currentApplication.rsvp && (
+												<Box spaceY="1rem">
+													<Flex alignItems="center" gapX="1rem">
+														<Text color="fg.muted">
+															Congrats, you’re in! Make sure to RSVP to claim
+															your spot!
+														</Text>
+														<Icon size="lg" color="green.400">
+															<CheckCircle />
+														</Icon>
+													</Flex>
+													<Text color="fg.muted">
+														See you at SpurHacks 2025 :)
+													</Text>
+												</Box>
+											)}
+										{!!currentApplication &&
+											currentApplication.applicationStatus !== "accepted" && (
+												<Box spaceY="1rem">
+													<Text color="fg.muted">
+														Thank you for applying to SpurHacks 2025. We were
+														impressed with your application, but we are unable
+														to offer a hacker acceptance at this time.
+													</Text>
+													<Text color="fg.muted">We wish you the best!</Text>
+												</Box>
+											)}
+									</Box>
+								)}
+							</Card.Body>
+							<Card.Footer>
+								{deadlines.inRange && !currentApplication && (
+									<Button
+										color="black"
+										colorScheme="brand"
+										onClick={() => navigate(paths.apply)}
+										marginLeft="auto"
+										size="lg"
+										rounded="full"
+										marginTop="1rem"
+										textTransform="uppercase"
+									>
+										Apply Now
+									</Button>
+								)}
+								{deadlines.afterClose &&
+									!!currentApplication &&
+									currentApplication.applicationStatus === "accepted" &&
+									!currentApplication.rsvp && (
+										<Button
+											color="black"
+											colorScheme="brand"
+											onClick={() => console.log("not implemented")}
+											marginLeft="auto"
+											size="lg"
+											rounded="full"
+											marginTop="1rem"
+											textTransform="uppercase"
+										>
+											RSVP
+										</Button>
+									)}
+							</Card.Footer>
+						</Card.Root>
+					)}
 
 					{!isLoadingTypeforms && typeforms?.mjvURL && (
 						<Card.Root
