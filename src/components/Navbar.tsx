6c19a76525893059c9ed612d7611f4bda1111458
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-06-01 01:55:59 -0400

    [add] my account navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,7 +28,6 @@ import {
 	ShareIcon,
 	TicketIcon,
 	UserGroupIcon,
-	Cog8ToothIcon,
 } from "@heroicons/react/24/outline";
 import Hamburger from "hamburger-react";
 import { useEffect, useMemo, useState } from "react";
@@ -66,10 +65,6 @@ const navItems = {
 	[paths.perks]: {
 		label: "Perks",
 		Icon: RxStar,
-	},
-	[paths.accountSettings]: {
-		label: "Account",
-		Icon: Cog8ToothIcon,
 	},
 };
 
