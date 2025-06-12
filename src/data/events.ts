66e7962db3f8c11ad4bf55b9af4c62d2c43e2f62
Aidan Traboulay <aidantraboulay13@gmail.com>
2025-06-12 13:51:14 -0400

    add temp event data

---
+++ src/data/events.ts
@@ -0,0 +1,161 @@
+export const schedule = [
+	// Friday June 20th events
+	{
+		id: "fri-registration",
+		startDate: new Date("2025-06-20T09:00:00"),
+		endTime: new Date("2025-06-20T12:00:00"),
+		color: "teal",
+		title: "Registration",
+		location: "Main Lazeez",
+		description:
+			"Reprehenderit necessitatibus aliquid iste iure molestiae eres sunt quia voluptate provident et. Dolore velit tenetur hic suscipit ea at. Sed et sed ut. Libero doloremque in ullam nemo ea quibusdam quia ipsa. Repellat unde voluptatem quisquam. Perspiciatis nesciunt id aperiam.",
+		row: 1,
+	},
+	{
+		id: "fri-mimosas",
+		startDate: new Date("2025-06-20T08:00:00"),
+		endTime: new Date("2025-06-20T11:00:00"),
+		color: "purple",
+		title: "Morning Beers",
+		location: "Lazeez Area",
+		description:
+            "Reprehenderit necessitatibus aliqtia aliquid et omnis. Ipsam expedita aut dolores sunt quia voluptate provident et. Dolore velit tenetur hic suscipit ea at. Sed et sed ut. Libero doloremque in ullam nemo ea quibusdam quia ipsa. Repellat unde voluptatem quisquam. Perspiciatis nesciunt id aperiam.",
+		row: 2,
+	},
+
+	// Saturday June 21st events
+	{
+		id: "sat-speaker",
+		startDate: new Date("2025-06-21T12:00:00"),
+		endTime: new Date("2025-06-21T13:00:00"),
+		color: "orange",
+		title: "Guest Speaker",
+		location: "Lazeez Hall",
+		description:
+            "Reprehenderit necessitatibus aliquid iste iure molestiae eum. Sapiente atque mollitia aliquid et omnis. Ipsam expedita aut dolores sunt quia voluptate provident et. Dolore velit tenetur hic suscipit ea at. Sed et sed ut. Libero doloremque in ullam nemo ea quibusdam quia ipsa. Repellat unde voluptatem quisquam. Perspiciatis nesciunt id aperiam.",
+		row: 1,
+	},
+	{
+		id: "sat-yoga",
+		startDate: new Date("2025-06-21T11:00:00"),
+		endTime: new Date("2025-06-21T12:00:00"),
+		color: "purple",
+		title: "PP Swinging",
+		location: "Lazeez Studio",
+		description:
+            "Reprehenderit necessitatibus aliqaut dolores sunt quia voluptate provident et. Dolore velit tenetur hic suscipit ea at. Sed et sed ut. Libero doloremque in ullam nemo ea quibusdam quia ipsa. Repellat unde voluptatem quisquam. Perspiciatis nesciunt id aperiam.",
+		row: 2,
+	},
+	{
+		id: "sat-brunch",
+		startDate: new Date("2025-06-21T11:00:00"),
+		endTime: new Date("2025-06-21T14:00:00"),
+		color: "purple",
+		title: "Brunch Bros",
+		location: "Lazeez",
+		description:
+            "Reprehenderit necessitatibus aliquid iste iure molestiae eum. Sapiente atque mollitia aliquid et omnis. Ipsam expedita aut dolores sunt quia voluptate provident et. Dolore velit tenetur hic suscipit ea at. Sed et sed ut. Libero doloremque in ullam nemo ea quibusdam quia ipsa. Repellat unde voluptatem quisquam. Perspiciatis nesciunt id aperiam.",
+		row: 3,
+	},
+	{
+		id: "sat-ai-intro",
+		startDate: new Date("2025-06-21T13:00:00"),
+		endTime: new Date("2025-06-21T14:00:00"),
+		color: "blue",
+		title: "Intro to AI",
+		location: "Lazeez",
+		description:
+            "Reprehenderit necessitatibus aliquid iste iure molestiae eum. Sapiente atque mollitia aliquid et omnis. Ipsam expedita aut dolores sunt quia voluptate provident et. Dolore velit tenetur hic suscipit ea at. Sed et sed ut. Libero doloremque in ullam nemo ea quibusdam quia ipsa. Repellat unde voluptatem quisquam. Perspiciatis nesciunt id aperiam.",
+		row: 4,
+	},
+	{
+		id: "sat-web-design",
+		startDate: new Date("2025-06-21T14:00:00"),
+		endTime: new Date("2025-06-21T15:00:00"),
+		color: "blue",
+		title: "Web Design",
+		location: "Lazeezb",
+		description:
+            " sunt quia voluptate proviuisquam. Perspiciatis nesciunt id aperiam.",
+		row: 1,
+	},
+	{
+		id: "sat-blockchain",
+		startDate: new Date("2025-06-21T14:00:00"),
+		endTime: new Date("2025-06-21T15:00:00"),
+		color: "blue",
+		title: "Blockchain",
+		location: "Lazeez Room A - 1st Floor",
+		description:
+            "Reprehenderit necessitatibus aliquid iste iure molestiae eum. Sapiente atque mollitia aliquid et omnis. Ipsam expedita aut dolores sunt quia voluptate provident et. Dolore velit tenetur hic suscipit ea at. Sed et sed ut. Libero doloremque in ullam nemo ea quibusdam quia ipsa. Repellat unde voluptatem quisquam. Perspiciatis nesciunt id aperiam.",
+		row: 2,
+	},
+	{
+		id: "sat-jenga",
+		startDate: new Date("2025-06-21T14:00:00"),
+		endTime: new Date("2025-06-21T15:00:00"),
+		color: "purple",
+		title: "Jenga",
+		location: "Lazeez Room",
+		description:
+            "Reprehenderit necessitatibus aue mollitia aliquid et omnis. Ipsam expedita aut dolores sunt quia voluptate provident et. Dolore velit tenetur hic suscipit ea at. Sed et sed ut. Libero doloremque in ullam nemo ea quibusdam quia ipsa. Repellat unde voluptatem quisquam. Perspiciatis nesciunt id aperiam.",
+		row: 3,
+	},
+	{
+		id: "sat-networking",
+		startDate: new Date("2025-06-21T16:00:00"),
+		endTime: new Date("2025-06-21T18:00:00"),
+		color: "orange",
+		title: "Networking",
+		location: "Lazeez Hall",
+		description:
+            "Reprehenderit necessitatibus aliquid iste iure molestiae eum. Sapiente atque mollitia aliquid et omnis. Ipsam expedita aut dolores sunt quia voluptate provident et. Dolore velit tenetur hic suscipit ea at. Sed et sed ut. Libero doloremque in ullam nemo ea quibusdam quia ipsa. Repellat unde voluptatem quisquam. Perspiciatis nesciunt id aperiam.",
+		row: 1,
+	},
+	{
+		id: "sat-fireside",
+		startDate: new Date("2025-06-21T16:00:00"),
+		endTime: new Date("2025-06-21T17:00:00"),
+		color: "blue",
+		title: "Fireside Chat",
+		location: "Outdoor Lazeez",
+		description:
+            "Reprehenderit necessitatibus aliquid iste iure molestiae eum. Sapiente atque mollitia aliquid et omnis. Ipsam expedita aut dolores sunt quia voluptate provident et. Dolore velit tenetur hic suscipit ea at. Sed et sed ut. Libero doloremque in ullam nemo ea quibusdam quia ipsa. Repellat unde voluptatem quisquam. Perspiciatis nesciunt id aperiam.",
+		row: 2,
+	},
+	{
+		id: "sat-musical-chairs",
+		startDate: new Date("2025-06-21T19:00:00"),
+		endTime: new Date("2025-06-21T20:30:00"),
+		color: "purple",
+		title: "Musical Chairs",
+		location: "Lazeez Lazeez Lazeez",
+		description:
+            "Reprehenderit necessitatibus alique mollitia aliquid et omnis. Ipsam expedita aut dolores sunt quia voluptate provident et. Dolore velit tenetur hic suscipit ea at. Sed et sed ut. Libero doloremque in ullam nemo ea quibusdam quia ipsa. Repellat unde voluptatem quisquam. Perspiciatis nesciunt id aperiam.",
+		row: 1,
+	},
+
+	// Sunday June 22nd events
+	{
+		id: "sun-registration",
+		startDate: new Date("2025-06-22T09:30:00"),
+		endTime: new Date("2025-06-22T11:30:00"),
+		color: "teal",
+		title: "Registration",
+		location: "Lazeez Lazeez Lazeez",
+		description:
+            "Reprehenderit necessitatibus aliquid iste iure molestiae eum.",
+		row: 1,
+	},
+	{
+		id: "sun-closing",
+		startDate: new Date("2025-06-22T10:00:00"),
+		endTime: new Date("2025-06-22T12:00:00"),
+		color: "green",
+		title: "Closing Ceremony",
+		location: "Lazeez",
+		description:
+            "Reprehenderit necessitatibus aliquid iste iure molestiae eum. Sapiente atque mollitia aliquid et omnis. Ipsam expedita aut dolores sunt quia voluptate provident et. Dolore velit tenetur hic suscipit ea at. Sed et sed ut. Libero doloremque in ullam nemo ea quibusdam quia ipsa. Repellat unde voluptatem quisquam. Perspiciatis nesciunt id aperiam.",
+		row: 2,
+	},
+];
